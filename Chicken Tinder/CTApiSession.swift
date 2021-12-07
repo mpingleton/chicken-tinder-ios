07eed5cb20f6718e0133945594108d11508c010d
Michael Pingleton <mnpingleton@gmail.com>
2021-12-07 11:10:36 -0600

    Added initial members to class. Wrote example code for parsing and writing json data.

--- Chicken Tinder/CTApiSession.swift
+++ Chicken Tinder/CTApiSession.swift
@@ -5,6 +5,29 @@
 //  Created by Michael Pingleton on 12/6/21.
 //
 
+/*
+Below is example code for constructing a json object and then serializing it.
+
+ let loginCredentials: Dictionary = ["username": username, "passphrase": passphrase];
+ print(loginCredentials);
+ let jsonData = try! JSONSerialization.data(withJSONObject: loginCredentials, options: JSONSerialization.WritingOptions.prettyPrinted)
+ print(String(decoding: jsonData, as: UTF8.self))
+ 
+ let jsonObject = try! JSONSerialization.jsonObject(with: jsonData, options: JSONSerialization.ReadingOptions.mutableContainers)
+ print(jsonObject)
+ */
+
+import Foundation
+
 class CTApiSession {
     
+    var accessToken: String = ""
+    
+    func login(username: String, passphrase: String) {
+        
+    }
+    
+    func logout() {
+        
+    }
 }
